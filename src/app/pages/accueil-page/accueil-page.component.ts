58117ec8f29ffb5e56e9f29adaf6e75011540b24
barrymendy <barrymendy@gmail.com>
2024-04-19 12:32:03 +0200

    Creation navbar

--- src/app/pages/accueil-page/accueil-page.component.ts
+++ src/app/pages/accueil-page/accueil-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-accueil-page',
   standalone: true,
   imports: [
     NavbarComponent,
+    FooterComponent,
   ],
   templateUrl: './accueil-page.component.html',
   styleUrl: './accueil-page.component.css'
